ac23540a64e1d075b8d30b2ac590e9ae4e14a090
shiying <shiying.wei@ucl.ac.uk>
2025-02-24 12:45:37 +0000

    Setup Django routing

--- {{cookiecutter.project_slug}}/backend/{{cookiecutter.project_slug}}/urls.py
+++ {{cookiecutter.project_slug}}/backend/{{cookiecutter.project_slug}}/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework import routers
 
+router = routers.DefaultRouter()
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/',include('api.urls'))
+    path('api/', include(router.urls)),
 ]
